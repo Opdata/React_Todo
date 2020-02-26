37651df40eddba9abc315a1de44a54fb964fc55e
Opdata <pb03641@gmail.com>
2020-02-26 13:45:52 +0900

    [style] TodoTemplate 컴포넌트 styled-component 스타일링

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,14 +1,38 @@
 // 화면을 가운데에 정렬시켜주며, 일정관리를 보여준다. children 으로 내부 jsx를 props로 받아 렌더링해준다.
 
 import React from 'react';
-import classnames from 'classnames';
+import styled from 'styled-components';
+
+const Maindiv = styled.div`
+  width: 512px;
+  /* width가 주어진 상태에서 좌우 중앙 정렬*/
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6rem;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Title = styled.div`
+  background: #22b8cf;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Body = styled.div`
+  background: white;
+`;
 
 const TodoTemplate = ({ children }) => {
   return (
-    <div className='TodoTemplate'>
-      <div className='app-title'>일정 관리</div>
-      <div className='content'>{children}</div>
-    </div>
+    <Maindiv className='TodoTemplate'>
+      <Title className='app-title'>일정 관리</Title>
+      <Body className='content'>{children}</Body>
+    </Maindiv>
   );
 };
 
