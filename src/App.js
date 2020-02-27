1f077258d306b0d623ca936145407d9eb9432b38
Opdata <pb03641@gmail.com>
2020-02-27 19:29:20 +0900

    [feature] useCallback, useState 사용으로 Todo 추가시 객체로 추가되는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트 todo 1번째',
-      checked: false,
+  const [todos, setTodos] = useState([]);
+  const nextId = useRef(1);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
     },
-    {
-      id: 2,
-      text: '리액트 todo 2번째',
-      checked: true,
-    },
-  ]);
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
