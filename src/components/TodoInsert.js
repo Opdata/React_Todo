48c5daddca6ba4a0f8b023f7237ea0b317efca04
Opdata <pb03641@gmail.com>
2020-02-27 19:20:08 +0900

    [feature] useState, useCallback 사용으로 TodoInsert 기능 추가

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,6 +1,6 @@
 // 새로운 항목을 입력하고 추가할 수 있는 컴포넌트 state를 통해 인풋의 상태를 관리
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import { MdAdd } from 'react-icons/md';
 
@@ -42,9 +42,15 @@ const Button = styled.button`
 `;
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <MainForm>
-      <Input placeholder='할 일을 입력하세요' />
+      <Input placeholder='할 일을 입력하세요' value={value} onChange={onChange} />
       <Button type='submit'>
         <MdAdd />
       </Button>
