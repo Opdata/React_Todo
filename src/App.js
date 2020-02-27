9764cd614dba50d47e999aff35175eb1f735954b
Opdata <pb03641@gmail.com>
2020-02-28 02:47:53 +0900

    [feature] 데이터 2500개 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+//App.js
+
 import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 0; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const nextId = useRef(1);
+  const [todos, setTodos] = useState(createBulkTodos);
+  const nextId = useRef(2501);
   const onInsert = useCallback(
     text => {
       const todo = {
